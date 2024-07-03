ed0190b6b5251f975e72b1d184902250b0a03073
sunnydowari <86121971+sunnydowari@users.noreply.github.com>
2024-07-03 13:10:43 +0530

    client update(react router dom) and todo component

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,15 +1,25 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './pages/home/Home'
+import Login from './pages/login/Login'
+import Signup from './pages/signup/Signup'
+import User from './pages/user/User'
+import Navbar from './components/navbar/Navbar'
 
 function App() {
- 
-
   return (
-    <>
+    <Router>
       <div>
-        <h1>hello world</h1>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/user" element={<User />} />
+        </Routes>
       </div>
-      
-    </>
+    </Router>
   )
 }
 
