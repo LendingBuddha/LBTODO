127e7f1281b619cc8cabad46f1ecaa1b5f46f2c9
HarikaAltada <altadaharika55@gmail.com>
2024-07-10 18:30:40 +0530

    REFACTOR: boilerplate changes

--- app/src/components/navbar/Navbar.jsx
+++ app/src/components/navbar/Navbar.jsx
@@ -2,9 +2,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./navbar.css";
-import React from "react";
-import { Link } from "react-router-dom";
-import "./navbar.css";
 import logo from "../../assets/logo.png";
 
 function Navbar() {
@@ -30,9 +27,6 @@ function Navbar() {
           <Link to="/">Home</Link>
         </li>
         <li className="dropdown">
-          <Link to="/user" className="dropdown-toggle">
-            User
-          </Link>
           <Link
             to="/user"
             className="dropdown-toggle"
--- app/src/pages/home/Home.jsx
+++ app/src/pages/home/Home.jsx
@@ -1,60 +1,57 @@
 // src/pages/home/Home.js
-import React, { useState } from 'react';
-import './Home.css'
+import React, { useState } from "react";
+import "./Home.css";
 
 const Home = () => {
   const [tasks, setTasks] = useState([]);
-  const [newTask, setNewTask] = useState('');
-  const [newDate, setNewDate] = useState('');
+  const [newTask, setNewTask] = useState("");
+  const [newDate, setNewDate] = useState("");
 
   const addTask = () => {
     if (newTask && newDate) {
       setTasks([...tasks, { task: newTask, date: newDate }]);
-      setNewTask('');
-      setNewDate('');
+      setNewTask("");
+      setNewDate("");
     }
   };
-
- 
 
   const deleteTask = (index) => {
     const updatedTasks = tasks.filter((_, i) => i !== index);
     setTasks(updatedTasks);
   };
-  
 
   return (
     <>
-    <div className="home">
-      <div className="todo-container">
-        <h2>To Do List</h2>
-        <div className="input-container">
-          <input
-            type="text"
-            placeholder="New Task"
-            value={newTask}
-            onChange={(e) => setNewTask(e.target.value)}
-          />
-          <input
-            type="date"
-            value={newDate}
-            onChange={(e) => setNewDate(e.target.value)}
-          />
-          <button onClick={addTask}>Add</button>
-        </div>
-        <div className="task-list">
-          {tasks.map((item, index) => (
-            <div key={index} className="task-item">
-              <span>{item.task}</span>
-              <span>{item.date}</span>
-              <button onClick={() => deleteTask(index)}>Delete</button>
-            </div>
-          ))}
+      <div className="home">
+        <div className="todo-container">
+          <h2>To Do List</h2>
+          <div className="input-container">
+            <input
+              type="text"
+              placeholder="New Task"
+              value={newTask}
+              onChange={(e) => setNewTask(e.target.value)}
+            />
+            <input
+              type="date"
+              value={newDate}
+              onChange={(e) => setNewDate(e.target.value)}
+            />
+            <button onClick={addTask}>Add</button>
+          </div>
+          <div className="task-list">
+            {tasks.map((item, index) => (
+              <div key={index} className="task-item">
+                <span>{item.task}</span>
+                <span>{item.date}</span>
+                <button onClick={() => deleteTask(index)}>Delete</button>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
-}
+};
 
 export default Home;
