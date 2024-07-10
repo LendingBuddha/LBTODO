61ee4c610ab751b4e40654737c522408ecf3186d
Akshat Gupta <agdg1313@gmail.com>
2024-07-10 16:00:29 +0530

    BUILD: added mongodb connection

--- app/src/pages/home/Home.jsx
+++ app/src/pages/home/Home.jsx
@@ -1,12 +1,40 @@
 // src/pages/home/Home.js
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './Home.module.css'
+import axios from 'axios'
 
 function Home() {
+  const [todos, setTodos] = useState([])
+  useEffect(() => {
+    axios.get('http://localhost:3000/get')
+    .then(result => setTodos(result.data))
+    .catch(err => console.log(err))
+  }, [])
+  const [task, setTask] =useState()
+  const handleAdd = () => {
+    axios.post('http://localhost:3000/add', {task: task})
+    .then(result => console.log(result))
+    .catch(err => console.log(err))
+  }
   return (
     <div className={styles.home}>
-      <h1>Home Page</h1>
-      {/* Your content */}
+      <p>hello world!!</p>
+      <h2>Todo</h2>
+      <div>
+        <input type='text' placeholder='Enter Task' onChange={(e) => setTask(e.target.value)}/>
+        <button type='button' onClick={handleAdd}> Add</button>
+      </div>
+      {
+        todos.length === 0
+        ?
+        <div><h2> No Record</h2></div>
+        :
+        todos.map(todo => (
+          <div className={styles.task}>
+            {todo.task}
+          </ div>
+        ))
+      }
     </div>
   )
 }
--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import { UserRouter } from "./routes/auth.js";
 import cors from "cors";
+import noteModel from './model/notes.js';
 dotenv.config();
 const app = express();
 
@@ -16,9 +17,25 @@ app.use(
 );
 app.use(cookieParser());
 app.use("/auth", UserRouter);
+app.use(express.json())
 
-mongoose.connect("mongodb://127.0.0.1:27017/authentication");
+mongoose.connect("mongodb://127.0.0.1:27017/test");
 
+
+app.get('/get', (req,res) => {
+  noteModel.find()
+  .then(result => res.json(result))
+  .catch(err => res.json(err))
+})
+
+app.post('/add', (req,res) => {
+  const task = req.body.task;
+  noteModel.create({
+    task: task
+  }).then(result => res.json(result))
+    .catch(err => res.json(err))
+
+})
 app.listen(3000, () => {
   "server is running";
 });
